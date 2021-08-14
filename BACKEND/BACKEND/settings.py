3d34c30b71794cf01508049502ea1f8b4f8d8a07
AlphaTechnic <juho3471@gmail.com>
2021-08-14 23:40:16 +0900

    Hide a SECRET_KEY

--- BACKEND/BACKEND/settings.py
+++ BACKEND/BACKEND/settings.py
@@ -9,18 +9,28 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import environ, os
 from pathlib import Path
+
+# secret key 보호를 위한 작업
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# reading .env file
+environ.Env.read_env(
+    env_file=os.path.join(BASE_DIR, '.env')
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-967il-7j5szxj__25ukecftlt!7)_v+7yl@a^l6dsn$fv*az%u'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
