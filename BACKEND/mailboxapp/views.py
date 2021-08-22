a8e4f056b4fdfb8330bb17f4c043b7d38a96eb11
sw-develop <psw00719@naver.com>
2021-08-23 00:25:56 +0900

    [추가] get_serializer_class()에 모든 편지 반환용 serializer 조건 추가

--- BACKEND/mailboxapp/views.py
+++ BACKEND/mailboxapp/views.py
@@ -1,16 +1,13 @@
 import random
 import string
 
-from mailboxapp.models import Mailbox
+from datetime import date
 from requests import Response
 from rest_framework import viewsets, status
-from mailboxapp.serializers import CreateMailBoxSerializer, ListMailBoxSerializer, GetMailBoxSerializer, \
-    CheckMailBoxKeySerializer
-
-from datetime import date
-
 from rest_framework.decorators import action
-
+from mailboxapp.models import Mailbox
+from mailboxapp.serializers import CreateMailBoxSerializer, ListMailBoxSerializer, GetMailBoxSerializer
+from letterapp.serializers import ListLetterSerializer
 
 # ViewSet 사용
 # api 다 그냥 mailbox로 통일시켜버려... my-mailbox -> mailbox로 .. 그럼 한방에 처리 가능함!
@@ -31,25 +28,25 @@ def get_random_key():  # 랜덤 우체통 비밀키 값 생성 메서드
 
 
 class MailboxViewSet(viewsets.ModelViewSet):
-    # permission_classes 추가 필요
+    # Authentication & Permission 추가 필요
 
     # GenericAPIView클래스의 get_queryset() 메서드 오버라이딩
     def get_queryset(self):
         queryset = self.request.user.mailboxes.all()  # 특정 사용자와 연관된 모든 우체통 객체 반환
         return queryset
 
-    # Create, List Action
     # GenericAPIView클래스의 get_serializer_class() 메서드 오버라이딩 - 조건에 맞는 Serializer 반환
     def get_serializer_class(self):
         if self.action == 'create':
             return CreateMailBoxSerializer
-        if self.action == 'list':
+        elif self.http_method_names == 'get':
+            if self.name == 'get_letters':
+                return ListLetterSerializer
             return ListMailBoxSerializer
 
     """
     POST mailbox (우체통 생성) 
     """
-
     def perform_create_mailbox(self, request, serializer):
         # user, link_title, open_date, key 필드에 값 추가하기
         mailbox = serializer.save(
@@ -85,3 +82,9 @@ class MailboxViewSet(viewsets.ModelViewSet):
         if mailbox.check_mailbox_key(request.data['key']):  # 키 값이 일치한 경우
             return Response(status=status.HTTP_200_OK)
         return Response(status=status.HTTP_403_FORBIDDEN)
+
+    """
+    GET mailbox/<int:mailbox_pk>/letters - 특정 우체통 편지 조회 
+    """
+    @action(detail=True, methods=['get'], url_path='letters', name='get_letters')
+    def get_letters(self, request, pk=None):
