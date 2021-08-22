5146c08fdd7940e6862be11c873037378fd160c3
sw-develop <psw00719@naver.com>
2021-08-22 16:18:29 +0900

    [추가] 특정 우체통 반환용 serializer 생성

--- BACKEND/mailboxapp/serializers.py
+++ BACKEND/mailboxapp/serializers.py
@@ -9,14 +9,19 @@ class CreateMailBoxSerializer(serializers.ModelSerializer):
         fields = ['nickname']
 
 
-def get_number_of_letter(obj):
-    return obj.number_of_letter()
+class GetMailBoxSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MailBox
+        fields = '__all__'
 
 
-# 개인 우체통 조회 - mailbox_id, link_title, mailbox_link, open_date & 편지 개수
+# 개인의 모든 우체통 조회 - mailbox_id, link_title, mailbox_link, open_date & 편지 개수
 class ListMailBoxSerializer(serializers.ModelSerializer):
     number_of_letter = serializers.SerializerMethodField()
 
     class Meta:
         model = MailBox
         fields = ['id', 'link_title', 'mailbox_link', 'open_date', 'number_of_letter']
+
+    def get_number_of_letter(obj):
+        return obj.number_of_letter()
