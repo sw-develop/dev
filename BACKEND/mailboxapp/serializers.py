448f59c89baf8c2a1b32d217fe0a681a02efa731
sw-develop <psw00719@naver.com>
2021-08-22 13:55:39 +0900

    [수정] 편지 개수 반환 메서드 static 메서드로 선언

--- BACKEND/mailboxapp/serializers.py
+++ BACKEND/mailboxapp/serializers.py
@@ -9,6 +9,10 @@ class CreateMailBoxSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+def get_number_of_letter(obj):
+    return obj.number_of_letter()
+
+
 # 개인 우체통 조회 - mailbox_id, link_title, mailbox_link, open_date & 편지 개수
 class ListMailBoxSerializer(serializers.ModelSerializer):
     number_of_letter = serializers.SerializerMethodField()
@@ -16,6 +20,3 @@ class ListMailBoxSerializer(serializers.ModelSerializer):
     class Meta:
         model = MailBox
         fields = ['id', 'link_title', 'mailbox_link', 'open_date', 'number_of_letter']
-
-    def get_number_of_letter(self, obj):
-        return obj.number_of_letter()
