b303505f9a9c873c479d766a9b824439de1989bc
sw-develop <psw00719@naver.com>
2021-08-22 15:49:55 +0900

    [추가] 랜덤 우체통 공개 날짜, 랜덤 우체통 비밀키 값 생성 메서드

--- BACKEND/mailboxapp/views.py
+++ BACKEND/mailboxapp/views.py
@@ -1,20 +1,40 @@
+import random
+import string
+
 from django.shortcuts import render
 from mailboxapp.models import Mailbox
-from rest_framework import viewsets
+from requests import Response
+from rest_framework import viewsets, status
 from mailboxapp.serializers import CreateMailBoxSerializer, ListMailBoxSerializer
+
+from datetime import date
+
 
 # ViewSet 사용
 # api 다 그냥 mailbox로 통일시켜버려... my-mailbox -> mailbox로 .. 그럼 한방에 처리 가능함!
 
 
-class MailboxViewSet(viewsets.ModelViewSet):
-    queryset = Mailbox.objects.all()
+def get_random_open_date(): # 랜덤 우체통 공개 날짜 생성 메서드
+    # 랜덤 날짜 조건 : 우체통 봉인 시점(우체통 생성 후 3일 뒤)부터 1주일 ~ 1달 후
+    mailbox_close_date = date.today() + 3
+    return mailbox_close_date + random.randint(7, 30)
 
+
+def get_random_key(): # 랜덤 우체통 비밀키 값 생성 메서드
+    key_length = 6
+    string_pool = string.ascii_lowercase # 소문자
+    secret_key = ""
+    for i in range(key_length):
+        secret_key += random.choice(string_pool)
+    return secret_key
+
+
+class MailboxViewSet(viewsets.ModelViewSet):
     # permission_classes 추가 필요
 
     # GenericAPIView클래스의 get_queryset() 메서드 오버라이딩
     def get_queryset(self):
-        queryset = self.request.user.mailboxes.all() # 특정 사용자와 연관된 모든 우체통 객체 반환
+        queryset = self.request.user.mailboxes.all()  # 특정 사용자와 연관된 모든 우체통 객체 반환
         return queryset
 
     # Create, List Action
@@ -25,8 +45,28 @@ class MailboxViewSet(viewsets.ModelViewSet):
         if self.action == 'list':
             return ListMailBoxSerializer
 
-    def list(self, request):
+    """
+    POST mailbox/
+    """
 
+    def perform_create_mailbox(self, request, serializer):
+        # user, link_title, open_date, key 필드에 값 추가하기
+        serializer.save(
+            user=request.user,  # 추가 - Authentication 추가해야 사용 가능함
+            link_title=request.data['nickname'] + '의 우체통',
+            open_date=get_random_open_date()
+            key=
+        )
 
+        # mailbox_link 필드 업데이트
 
-    # POST mailbox/<int:mailbox_pk>/secretkey
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create_mailbox(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    """
+    POST mailbox/<int:mailbox_pk>/secretkey
+    """
