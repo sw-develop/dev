15bf7ab32de6880a1a4851c518c5c73788f6bd8e
sw-develop <psw00719@naver.com>
2021-08-23 21:03:21 +0900

    [수정] key 필드 null 허용

--- BACKEND/mailboxapp/models.py
+++ BACKEND/mailboxapp/models.py
@@ -9,7 +9,7 @@ class MailBox(models.Model):
     link_title = models.CharField(max_length=40)
     mailbox_link = models.URLField(null=True)  # default max_length = 200, 형태 = mailbox/<int:mailbox_pk>/letter
     open_date = models.DateField()  # 우체통 공개 날짜
-    key = models.CharField(max_length=50, db_column="mailbox_key")  # 우체통 비밀키
+    key = models.CharField(max_length=50, db_column="mailbox_key", null=True)  # 우체통 비밀키
 
     ThemeType = models.TextChoices('ThemeType', 'RED YELLOW ORANGE')  # 수정 - value 값 변경해야 함
     theme = models.CharField(max_length=20, choices=ThemeType.choices, null=True)
