fa4588c3373bd4272589af086f79ea380d2503a4
AlphaTechnic <juho3471@gmail.com>
2021-08-23 22:16:30 +0900

    [수정] 도커 시크릿에 비밀정보들을 숨김. 이에 따라 diploy.py 수정됨

--- BACKEND/BACKEND/settings/deploy.py
+++ BACKEND/BACKEND/settings/deploy.py
@@ -1,5 +1,13 @@
 from .base import *
 
+
+def read_secret(secret_name):
+    file = open("/run/secrets/" + secret_name)
+    secret = file.read()
+    secret = secret.rstrip().lstrip()
+    file.close()
+    return secret
+
 # secret key 보호를 위한 작업
 env = environ.Env(
     # set casting, default value
@@ -15,7 +23,7 @@ environ.Env.read_env(
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = read_secret("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -30,7 +38,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'poppymail_db',
         'USER': 'root',
-        'PASSWORD': 'admin',
+        'PASSWORD': read_secret("MYSQL_PASSWORD"),
         'HOST': 'mariadb',
         'PORT': '3306',
     }
