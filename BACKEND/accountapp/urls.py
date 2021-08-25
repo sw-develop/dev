c32d2f14732792256e56aa1a0ac643f4a72b7b07
sw-develop <psw00719@naver.com>
2021-08-25 22:45:09 +0900

    [추가] simple-jwt 라이브러리 제공 url

--- BACKEND/accountapp/urls.py
+++ BACKEND/accountapp/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from accountapp.views import KakaoLoginView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 app_name = "accountapp"
 
 urlpatterns = [
-    path('login/kakao/', KakaoLoginView.as_view(), name="login"),
+    # path('login/kakao/', KakaoLoginView.as_view(), name="login"),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
