61a4f83521a8559ab69dbb7dba28ec876948e45d
danbom <52441697+danbom@users.noreply.github.com>
2021-08-25 22:22:29 +0900

    FE update
    - setName, setGender 수정
    - 회원가입 폼 알림 추가

--- FRONTEND/src/components/JoinInput.js
+++ FRONTEND/src/components/JoinInput.js
@@ -1,67 +1,85 @@
-import React, { useContext, useState } from 'react';
-import { UserContext } from '../pages/JoinInfoPage';
-import {DropdownButton, Dropdown} from 'react-bootstrap';
+import React, { useContext, useState } from "react";
+import { UserContext } from "../pages/JoinInfoPage";
+import { DropdownButton, Dropdown } from "react-bootstrap";
 // import * as S from './styles';
 
 function JoinInput() {
+  // const [name, setName] = useState("");
+  // const [birthdate, setBirthdate] = useState("");
+  // const [gender, setGender] = useState("");
+  // const [phone, setPhone] = useState("");
+  const { setName, setBirthdate, setGender, setPhone } =
+    useContext(UserContext);
 
-    // const [name, setName] = useState("");
-    // const [birthdate, setBirthdate] = useState("");
-    // const [gender, setGender] = useState("");
-    // const [phone, setPhone] = useState("");
-    const { setName, setBirthdate, setGender, setPhone } = useContext(UserContext);
+  const processName = e => {
+    setName(e.target.value);
+  };
 
-    const processName = e => {
-        setName(e.target.value);
-    };
+  const processBirthdate = e => {
+    setBirthdate(e.target.value);
+  };
 
-    const processBirthdate = e => {
-        setBirthdate(e.target.value);
-    };
+  const setMan = e => {
+    setGender("남");
+  };
 
-    const setMan = e => {
-        setGender("남자");
-    };
+  const setWoman = e => {
+    setGender("여");
+  };
 
-    const setWoman = e => {
-        setGender("여자");
-    };
+  const setNone = e => {
+    setGender("");
+  };
 
-    const processPhone = e => {
-        setPhone(e.target.value);
-    };
+  const processPhone = e => {
+    setPhone(e.target.value);
+  };
 
-    return (
-        <>
-            <div className="joininput-name">
-                <div className="input-title">
-                    이름 </div>
-                <input className="input-name" placeholder="김파피" onChange={processName}></input>
-            </div>
+  return (
+    <>
+      <div className="joininput-name">
+        <div className="input-title">이름 </div>
+        <input
+          className="input-name"
+          placeholder="김파피"
+          onChange={processName}
+        ></input>
+      </div>
 
-            <div className="joininput-birth">
-                <div className="birth-title">
-                    생년월일 </div>
-                <input className="input-birth" placeholder="20001018" onChange={processBirthdate}></input>
-            </div>
+      <div className="joininput-birth">
+        <div className="birth-title">생년월일 </div>
+        <input
+          className="input-birth"
+          placeholder="20001018"
+          onChange={processBirthdate}
+        ></input>
+      </div>
 
-            <div className="joininput-sex">
-            <div className="sex-title">
-                    성별 </div>
-                <DropdownButton id="dropdown-basic-button" title="남자">
-                    <Dropdown.Item eventKey="item1" onClick={setMan}>남자</Dropdown.Item>
-                    <Dropdown.Item eventKey="item2" onClick={setWoman}>여자</Dropdown.Item>
-                </DropdownButton>
-            </div>
+      <div className="joininput-sex">
+        <div className="sex-title">성별 </div>
+        <DropdownButton id="dropdown-basic-button" title="남">
+          <Dropdown.Item eventKey="item1" onClick={setMan}>
+            남
+          </Dropdown.Item>
+          <Dropdown.Item eventKey="item2" onClick={setWoman}>
+            여
+          </Dropdown.Item>
+          <Dropdown.Item eventKey="item2" onClick={setNone}>
+            선택하지 않음
+          </Dropdown.Item>
+        </DropdownButton>
+      </div>
 
-            <div className="joininput-phone">
-                <div className="phone-title">
-                    핸드폰번호 </div>
-                <input className="input-phone" placeholder="01000000000" onChange={processPhone}></input>
-            </div>
-
-        </>
-    );
+      <div className="joininput-phone">
+        <div className="phone-title">핸드폰번호 </div>
+        <input
+          className="input-phone"
+          placeholder="01000000000"
+          onChange={processPhone}
+        ></input>
+      </div>
+    </>
+  );
 }
 
 export default JoinInput;
--- FRONTEND/src/pages/JoinInfoPage/index.js
+++ FRONTEND/src/pages/JoinInfoPage/index.js
@@ -1,79 +1,77 @@
-import React, { createContext, useState, useMemo } from 'react';
-import { Link, useHistory } from 'react-router-dom';
-import JoinInfoMent from '../../components/Txt/JoinInfoMent';
-import LogoNameJoin from '../../components/Txt/LogoNameJoin';
-import CompleteBtn from '../../components/Btn/CompleteBtn';
+import React, { createContext, useState, useMemo } from "react";
+import { Link, useHistory } from "react-router-dom";
+import JoinInfoMent from "../../components/Txt/JoinInfoMent";
+import LogoNameJoin from "../../components/Txt/LogoNameJoin";
+import CompleteBtn from "../../components/Btn/CompleteBtn";
 
-import * as S from './styles';
-import JoinInput from '../../components/JoinInput';
+import * as S from "./styles";
+import JoinInput from "../../components/JoinInput";
 
 export const UserContext = createContext({
-    setNickname: () => { },
-    setBirthdate: () => { },
-    setGender: () => { },
-    setPhone: () => { }
-})
+  setName: () => {},
+  setBirthdate: () => {},
+  setGender: () => {},
+  setPhone: () => {},
+});
 
 function JoinInfoPage() {
+  const history = useHistory();
 
-    const history = useHistory()
+  const [name, setName] = useState("");
+  const [birthdate, setBirthdate] = useState("");
+  const [gender, setGender] = useState("남");
+  const [phone, setPhone] = useState("");
+  const value = useMemo(
+    () => ({ setName, setBirthdate, setGender, setPhone }),
+    [setName, setBirthdate, setGender, setPhone]
+  );
 
-    const [nickname, setNickname] = useState("");
-    const [birthdate, setBirthdate] = useState("");
-    const [gender, setGender] = useState("남자");
-    const [phone, setPhone] = useState("");
-    const value = useMemo(() => ({ setNickname, setBirthdate, setGender, setPhone }), [setNickname, setBirthdate, setGender, setPhone]);
+  const JoinRequest = () => {
+    if (name === "" || birthdate === "" || gender === "" || phone === "") {
+      alert("필수 입력 요소가 작성되지 않았습니다 ... 알림창 만드러야댐");
+    } else {
+      alert(name + birthdate + gender + phone);
+      history.push("/joincomplete");
+    }
+    // fetch('http://158.247.195.25/sign_in/', {
+    //     method: "POST",
+    //     headers: {
 
-    const JoinRequest = () => {
-        if (nickname==="" || birthdate==="" || gender==="" || phone===""){
-            alert("필수 입력 요소가 작성되지 않았습니다 ... 알림창 만드러야댐");
-        }
-        else {
-            alert(nickname + birthdate + gender + phone);
-            history.push("/joincomplete");
-        }
-        // fetch('http://158.247.195.25/sign_in/', {
-        //     method: "POST",
-        //     headers: {
-                
-        //     },
-        //     body: JSON.stringify({
-        //         'name': nickname,
-        //         'birthdate': birthdate,
-        //         'gender': gender,
-        //         'phone': phone,
-        //     }),
-        // })
-        //     .then(res => res.json())
-        //     .then(res => {
-        //         // localStorage.setItem("Kakao_token", res.access_token);
-        //         // const kakao_token = localStorage.getItem("Kakao_token");
-        //         if (res) {
-        //             console.log(res);
-        //             // alert(res.user_name + "님, poppy mail에 오신 것을 환영합니다!");
-        //             // history.push("/joininfo");
-        //         }
-        //     })
-    };
+    //     },
+    //     body: JSON.stringify({
+    //         'name': nickname,
+    //         'birthdate': birthdate,
+    //         'gender': gender,
+    //         'phone': phone,
+    //     }),
+    // })
+    //     .then(res => res.json())
+    //     .then(res => {
+    //         // localStorage.setItem("Kakao_token", res.access_token);
+    //         // const kakao_token = localStorage.getItem("Kakao_token");
+    //         if (res) {
+    //             console.log(res);
+    //             // alert(res.user_name + "님, poppy mail에 오신 것을 환영합니다!");
+    //             // history.push("/joininfo");
+    //         }
+    //     })
+  };
 
-    return (
-        <UserContext.Provider value={value}>
-            <S.JoinInfoScene>
+  return (
+    <UserContext.Provider value={value}>
+      <S.JoinInfoScene>
+        <LogoNameJoin></LogoNameJoin>
+        <JoinInfoMent></JoinInfoMent>
 
-                <LogoNameJoin></LogoNameJoin>
-                <JoinInfoMent></JoinInfoMent>
+        <JoinInput></JoinInput>
 
-                <JoinInput></JoinInput>
-
-                {/* <CompleteBtn onClick={test}></CompleteBtn> */}
-                <div className="create-post-box-btn" onClick={JoinRequest}>
-                    완료
-                </div>
-
-
-            </S.JoinInfoScene>
-        </UserContext.Provider>
-    );
+        {/* <CompleteBtn onClick={test}></CompleteBtn> */}
+        <div className="create-post-box-btn" onClick={JoinRequest}>
+          완료
+        </div>
+      </S.JoinInfoScene>
+    </UserContext.Provider>
+  );
 }
 
 export default JoinInfoPage;
