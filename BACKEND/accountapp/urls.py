54115c0963ab504ed90b37c309d4731f7194285c
sw-develop <psw00719@naver.com>
2021-08-26 16:40:28 +0900

    [추가] user login url

--- BACKEND/accountapp/urls.py
+++ BACKEND/accountapp/urls.py
@@ -4,6 +4,9 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from .views import (
+    LoginView
+)
 
 app_name = "accountapp"
 
@@ -11,4 +14,5 @@ urlpatterns = [
     # path('login/kakao/', KakaoLoginView.as_view(), name="login"),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('account/login', LoginView.as_view(), name='user_login'),
 ]
