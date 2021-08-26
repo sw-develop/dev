5c9bf2c0d0c2a746ade8e5ef54e9d869a0791e6e
sw-develop <psw00719@naver.com>
2021-08-26 19:05:44 +0900

    [추가] 사용자 정보 추가 입력 url

--- BACKEND/accountapp/urls.py
+++ BACKEND/accountapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from accountapp.views import KakaoLoginView
+from accountapp.views import KakaoLoginView, AddUserInfoView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -14,5 +14,6 @@ urlpatterns = [
     path('login/kakao/', KakaoLoginView.as_view(), name="login"),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('account/login', LoginView.as_view(), name='user_login'),
+    path('account/login/', LoginView.as_view(), name='user_login'),
+    path('account/userInfo/', AddUserInfoView.as_view(), name='user_info'),
 ]
