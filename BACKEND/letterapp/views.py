cb3900c679c84b8265aa6874f946509cb3afabc1
AlphaTechnic <juho3471@gmail.com>
2021-08-29 01:20:22 +0900

    [생성] LetterRequestView의 get 메써드 작성

--- BACKEND/letterapp/views.py
+++ BACKEND/letterapp/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+from requests import Response
+from rest_framework import status
+
+from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
+from mailboxapp.models import MailBox
+
+
+class LetterRequestView(APIView):
+    permission_classes = [AllowAny]
+
+    # 메일박스에서 pk있는지 뒤져서 있으면 -> ok, 없으면 -> no such mailbox in DB
+    def get(self, request, mailbox_pk):
+        random_strkey = request.GET['key']
+        # print(random_strkey)
+
+        try:
+            obj = MailBox.objects.get(id=mailbox_pk, key=random_strkey)
+            return Response({'id': obj.id}, status=status.HTTP_200_OK)
+        except MailBox.DoesNotExist:
+            return Response({'No such a mailbox in DB'}, status=status.HTTP_204_NO_CONTENT)
+
+    #
+    # def post(self, request, mailbox_pk):
+
