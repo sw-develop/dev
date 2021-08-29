ecef59075c269a9a63f302ca7fa7a638545768a8
AlphaTechnic <juho3471@gmail.com>
2021-08-29 17:10:25 +0900

    [추가] HttpResponse에 상태코드 추가

--- BACKEND/letterapp/views.py
+++ BACKEND/letterapp/views.py
@@ -20,7 +20,7 @@ class LetterRequestView(APIView):
 
             # 우체통 동봉된 이후에 유저의 접근!!
             if datetime.date(datetime.now()) > mailbox_obj.open_date:
-                return HttpResponse("No! User accesses after mailbox is enclosed")
+                return HttpResponse("No! User accesses after mailbox has been enclosed", status=400)
 
             # ok response
             RESPONSE_DATA = dict()
@@ -29,7 +29,7 @@ class LetterRequestView(APIView):
 
             # 유저가 잘못된 url을 입력하여 접근 (url에 적힌 key가 DB에 없음)
         except MailBox.DoesNotExist:
-            return HttpResponse("No! The mailbox requested not exists in DB")
+            return HttpResponse("No! The mailbox requested not exists in DB", status=400)
 
     def post(self, request, mailbox_pk):
         random_strkey = request.GET['key']
@@ -45,8 +45,8 @@ class LetterRequestView(APIView):
                 color=request.data['color'],
             )
             letter_obj.save()
-            return HttpResponse("Ok! Saved in DB successfully")
+            return HttpResponse("Ok! Save in DB successfully")
 
             # 유저가 잘못된 url을 입력하여 접근 (url에 적힌 key가 DB에 없음)
         except MailBox.DoesNotExist:
-            return HttpResponse("No! The mailbox requested not exists in DB")
+            return HttpResponse("No! The mailbox requested not exists in DB", status=400)
