6343d4e3a3a130b8771b09d0178842b2ae2431d1
AlphaTechnic <juho3471@gmail.com>
2021-08-29 16:50:54 +0900

    [생성] 편지 작성 관련 get함수 post함수 작성

--- BACKEND/letterapp/views.py
+++ BACKEND/letterapp/views.py
@@ -1,28 +1,52 @@
-from django.shortcuts import render
-
-# Create your views here.
-from requests import Response
-from rest_framework import status
-
+from datetime import datetime
+from json import dumps
+from django.http import HttpResponse
 from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
+from letterapp.models import Letter
 from mailboxapp.models import MailBox
 
 
 class LetterRequestView(APIView):
     permission_classes = [AllowAny]
 
-    # 메일박스에서 pk있는지 뒤져서 있으면 -> ok, 없으면 -> no such mailbox in DB
+    # url에 적힌 key로 메일박스에서 해당 mailbox pk 있는지 뒤져서
+    # 있으면 -> ok, 없으면 -> no such mailbox in DB
     def get(self, request, mailbox_pk):
         random_strkey = request.GET['key']
-        # print(random_strkey)
 
         try:
-            obj = MailBox.objects.get(id=mailbox_pk, key=random_strkey)
-            return Response({'id': obj.id}, status=status.HTTP_200_OK)
+            mailbox_obj = MailBox.objects.get(id=mailbox_pk, key=random_strkey)
+
+            # 우체통 동봉된 이후에 유저의 접근!!
+            if datetime.date(datetime.now()) > mailbox_obj.open_date:
+                return HttpResponse("No! User accesses after mailbox is enclosed")
+
+            # ok response
+            RESPONSE_DATA = dict()
+            RESPONSE_DATA['mailbox_pk'] = mailbox_obj.id
+            return HttpResponse(dumps(RESPONSE_DATA), {'mailbox_pk': mailbox_obj.id})
+
+            # 유저가 잘못된 url을 입력하여 접근 (url에 적힌 key가 DB에 없음)
         except MailBox.DoesNotExist:
-            return Response({'No such a mailbox in DB'}, status=status.HTTP_204_NO_CONTENT)
+            return HttpResponse("No! The mailbox requested not exists in DB")
 
-    #
-    # def post(self, request, mailbox_pk):
+    def post(self, request, mailbox_pk):
+        random_strkey = request.GET['key']
 
+        try:
+            mailbox_obj = MailBox.objects.get(id=mailbox_pk, key=random_strkey)
+
+            # ok response
+            letter_obj = Letter.objects.create(
+                mailbox=mailbox_obj,
+                content=request.data['content'],
+                sender=request.data['sender'],
+                color=request.data['color'],
+            )
+            letter_obj.save()
+            return HttpResponse("Ok! Saved in DB successfully")
+
+            # 유저가 잘못된 url을 입력하여 접근 (url에 적힌 key가 DB에 없음)
+        except MailBox.DoesNotExist:
+            return HttpResponse("No! The mailbox requested not exists in DB")
