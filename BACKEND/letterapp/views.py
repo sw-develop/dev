ce57755b1585a50410b49f7f0e4ba5482cf490f4
AlphaTechnic <juho3471@gmail.com>
2021-09-01 19:13:20 +0900

    [수정] letterapp/views.py http response가 json response로 예쁘게 되도록 수정

--- BACKEND/letterapp/views.py
+++ BACKEND/letterapp/views.py
@@ -18,7 +18,11 @@ class LetterRequestView(APIView):
 
             # 우체통 동봉된 이후에 유저의 접근!!
             if datetime.now() > mailbox_obj.date_created + timedelta(hours=72):
-                return HttpResponse("No! User accesses after mailbox has been enclosed", status=400)
+                return HttpResponse(
+                    "No! User accesses after mailbox has been enclosed",
+                    content_type=u"application/json; charset=utf-8",
+                    status=400
+                )
 
             # ok response
             RESPONSE_DATA = dict()
@@ -32,7 +36,11 @@ class LetterRequestView(APIView):
 
             # 유저가 잘못된 url을 입력하여 접근 (url에 적힌 key가 DB에 없음)
         except MailBox.DoesNotExist:
-            return HttpResponse("No! The mailbox requested not exists in DB", status=400)
+            return HttpResponse(
+                "No! The requested mailbox not exists in DB",
+                content_type=u"application/json; charset=utf-8",
+                status=400
+            )
 
     def post(self, request, mailbox_pk):
         random_strkey = request.GET['key']
@@ -48,8 +56,15 @@ class LetterRequestView(APIView):
                 color=request.data['color'],
             )
             letter_obj.save()
-            return HttpResponse("Ok! Save in DB successfully")
+            return HttpResponse(
+                "Ok! Save in DB successfully",
+                content_type = u"application/json; charset=utf-8",
+            )
 
             # 유저가 잘못된 url을 입력하여 접근 (url에 적힌 key가 DB에 없음)
         except MailBox.DoesNotExist:
-            return HttpResponse("No! The mailbox requested not exists in DB", status=400)
+            return HttpResponse(
+                "No! The mailbox requested not exists in DB",
+                content_type=u"application/json; charset=utf-8",
+                status=400
+            )
