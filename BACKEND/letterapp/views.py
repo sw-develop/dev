092ba1689845e479439f9c86ce1509b51d6df240
sw-develop <psw00719@naver.com>
2021-09-06 23:18:18 +0900

    [추가] 특정 편지 읽음 처리 View

--- BACKEND/letterapp/views.py
+++ BACKEND/letterapp/views.py
@@ -1,7 +1,9 @@
 from datetime import datetime, timedelta
 from json import dumps
 from django.http import HttpResponse
+from rest_framework import status
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 from rest_framework.views import APIView
 from letterapp.models import Letter
 from mailboxapp.models import MailBox
@@ -61,3 +63,14 @@ class LetterRequestView(APIView):
                 "No! The requested mailbox not exists in DB",
                 status=400
             )
+
+
+class UpdateLetterView(APIView):  # 특정 편지 읽음 처리
+    # 인증 & 허가 - JWTAuthentication, IsAuthenticated (기본 설정)
+
+    def patch(self, request, letter_pk):
+        letter = Letter.objects.get(pk=letter_pk)
+        if letter.checked is not True:
+            letter.checked = True
+            letter.save()
+        return Response(status=status.HTTP_200_OK)
